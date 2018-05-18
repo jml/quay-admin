8724f595d9aca70b7e79560f69bdd726ed887691
Jonathan Lange <jml@mumak.net>
2018-05-18 20:34:19 +0100

    Switch to treq for parallelism

--- quay_admin.py
+++ quay_admin.py
@@ -1,51 +1,57 @@
+import json
 import os
-from pprint import pprint
-import time
+import sys
+
+import certifi
+os.environ["SSL_CERT_FILE"] = certifi.where()
 
 import attr
-import requests
+import treq
+from twisted.internet.defer import gatherResults, inlineCallbacks, returnValue
+from twisted.internet.task import react
 
 
 QUAY_IO_ENDPOINT = 'https://quay.io/api/v1'
 QUAY_TOKEN_ENV_NAME = 'QUAY_TOKEN'
-
-
-@attr.s
-class BearerToken(requests.auth.AuthBase):
-    """Bearer token authorization."""
-    token = attr.ib()
-
-    def __call__(self, r):
-        r.headers['Authorization'] = 'Bearer %s' % (self.token,)
-        return r
+STATE_FILE = 'quay.state'
 
 
 @attr.s
 class Registry(object):
     """A quay.io registry."""
     endpoint = attr.ib(default=QUAY_IO_ENDPOINT)
-    auth = attr.ib(default=None)
+    token = attr.ib(default=None)
 
+    def _request(self, method, path, headers=None, **kwargs):
+        url = '%s/%s' % (self.endpoint, path)
+        headers = headers if headers else {}
+        if self.token:
+            headers['Authorization'] = 'Bearer %s' % (self.token,)
+        return treq.request(method, url, headers=headers, **kwargs).addCallback(treq.json_content)
+
+    @inlineCallbacks
     def list_repositories(self, namespace):
         """List all the repositories in a given namespace.
 
         Ignores pagination completely.
         """
-        url = '%s/repository' % self.endpoint
-        repos = requests.get(
-            url, params={'namespace': namespace}, auth=self.auth).json()
-        return [Repository(**repo) for repo in repos['repositories']]
+        repos = yield self._request(
+            'GET', 'repository', params={'namespace': namespace})
+        returnValue([Repository(**repo) for repo in repos['repositories']])
 
+    @inlineCallbacks
     def get_user_permissions(self, repo_spec):
         """Get the user permissions for a repository."""
-        url = '%s/repository/%s/permissions/user/' % (self.endpoint, repo_spec)
-        perms = requests.get(url, auth=self.auth).json()
-        return map(UserPermission.from_dict, perms['permissions'].values())
+        path = 'repository/%s/permissions/user/' % (repo_spec,)
+        perms = yield self._request('GET', path)
+        returnValue(
+            map(UserPermission.from_dict, perms['permissions'].values()))
 
+    @inlineCallbacks
     def get_team_permissions(self, repo_spec):
-        url = '%s/repository/%s/permissions/team/' % (self.endpoint, repo_spec)
-        perms = requests.get(url, auth=self.auth).json()
-        return map(TeamPermission.from_dict, perms['permissions'].values())
+        path = 'repository/%s/permissions/team/' % (repo_spec,)
+        perms = yield self._request('GET', path)
+        returnValue(map(TeamPermission.from_dict, perms['permissions'].values()))
 
 
 @attr.s
@@ -62,6 +68,25 @@ class Repository(object):
     @property
     def spec(self):
         return '%s/%s' % (self.namespace, self.name)
+
+
+@inlineCallbacks
+def get_repository_permissions(repository, registry):
+    """Get the user and team permissions for a repository.
+
+    Returns a Deferred RepositoryPermissions.
+    """
+    [user_perms, team_perms] = yield gatherResults([
+        registry.get_user_permissions(repository.spec),
+        registry.get_team_permissions(repository.spec),
+    ])
+    returnValue(
+        RepositoryPermissions(
+            repository=repository,
+            user_permissions=user_perms,
+            team_permissions=team_perms,
+        )
+    )
 
 
 @attr.s
@@ -98,19 +123,37 @@ class TeamPermission(Permission):
     """A permission a team has."""
 
 
-def main():
+@attr.s
+class RepositoryPermissions(object):
+    """The permissions for a repository."""
+
+    repository = attr.ib()
+    user_permissions = attr.ib()
+    team_permissions = attr.ib()
+
+
+def map_concurrently(f, xs, *args, **kwargs):
+    """Run 'f' concurrently over each 'x' in 'xs'.
+
+    Also passes through '*args' and '**kwargs'.
+
+    Assumes 'f' returns Deferred values.
+    """
+    deferreds = [f(x, *args, **kwargs) for x in xs]
+    return gatherResults(deferreds)
+
+
+@inlineCallbacks
+def main(reactor, *args):
     quay_token = os.environ.get(QUAY_TOKEN_ENV_NAME, None)
-    auth = BearerToken(token=quay_token) if quay_token else None
-    registry = Registry(auth=auth)
-    repos = registry.list_repositories('weaveworks')
-    for repo in repos:
-        user_perms = registry.get_user_permissions(repo.spec)
-        team_perms = registry.get_team_permissions(repo.spec)
-        print repo
-        pprint(user_perms)
-        pprint(team_perms)
-        time.sleep(2)
+    registry = Registry(endpoint=QUAY_IO_ENDPOINT, token=quay_token)
+    namespace = 'weaveworks'
+    repos = yield registry.list_repositories(namespace)
+    perms = yield map_concurrently(get_repository_permissions, repos, registry)
+    with open(STATE_FILE, 'w') as state_file:
+        json.dump([attr.asdict(perm) for perm in perms], state_file)
+    returnValue(None)
 
 
 if __name__ == '__main__':
-    main()
+    react(main, sys.argv[1:])
